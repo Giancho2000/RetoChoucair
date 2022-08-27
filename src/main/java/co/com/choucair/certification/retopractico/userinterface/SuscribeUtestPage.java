0de4454baa0c0df59b9d351620d7b7d90dcc4412
Giancarlo Reina <greina@lendingpoint.com>
2022-08-27 11:39:57 -0500

    adding comments

--- src/main/java/co/com/choucair/certification/retopractico/userinterface/SuscribeUtestPage.java
+++ src/main/java/co/com/choucair/certification/retopractico/userinterface/SuscribeUtestPage.java
@@ -14,41 +14,41 @@ public class SuscribeUtestPage extends PageObject{
     public static final Target INPUT_EMAIL = Target.the("Put the Email into the input")
             .located(By.id("email"));
 
-    public static final Target INPUT_MONTH = Target.the("Click on the button Join Today")
+    public static final Target INPUT_MONTH = Target.the("Put the Month into the input")
             .located(By.id("birthMonth"));
 
-    public static final Target INPUT_DAY = Target.the("Click on the button Join Today")
+    public static final Target INPUT_DAY = Target.the("Put the day into the input")
             .located(By.id("birthDay"));
 
-    public static final Target INPUT_YEAR = Target.the("Click on the button Join Today")
+    public static final Target INPUT_YEAR = Target.the("Put the year into the input")
             .located(By.id("birthYear"));
 
     /*public static final Target INPUT_LENGUAJES = Target.the("Click on the button Join Today")
             .located(By.xpath("//input[@class='ui-select-search']"));*/
 
-    public static final Target NEXT_LOCATION = Target.the("Click on the button Join Today")
+    public static final Target NEXT_LOCATION = Target.the("Click on the button next location")
             .located(By.xpath("//a[@class='btn btn-blue']"));
 
-    public static final Target CITY = Target.the("Click on the button Join Today")
+    public static final Target CITY = Target.the("fill the input City")
             .located(By.id("city"));
 
-    public static final Target CITY_TWO = Target.the("Click on the button Join Today")
+    public static final Target CITY_TWO = Target.the("Select the city")
             .located(By.xpath("//input[@ng-model='registrationData.city']"));
 
 
 
-    public static final Target STATE = Target.the("Click on the button Join Today")
+    public static final Target STATE = Target.the("Select the state")
             .located(By.xpath("//span[@class='ui-select-match-text']"));
 
 
-    public static final Target POSTAL_CODE = Target.the("Click on the button Join Today")
+    public static final Target POSTAL_CODE = Target.the("Put the postal code")
             .located(By.id("zip"));
 
 
-    public static final Target COUNTRY = Target.the("Click on the button Join Today")
+    public static final Target COUNTRY = Target.the("Select the contry into the input")
             .located(By.xpath("//div[@class='ui-select-match']"));
 
-    public static final Target NEXT_DEVICES = Target.the("Click on the button Join Today")
+    public static final Target NEXT_DEVICES = Target.the("Click on the button Next device")
             .located(By.xpath("//a[@class='btn btn-blue']"));
 
 }
